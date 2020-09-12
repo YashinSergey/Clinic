b93560c349d59acbe3798fbd556739e0545ede09
SergeyYashin <yashinsergey125@gmail.com>
2020-09-12 19:31:27 +0300

    Added CLINIC-002 Добавлен CalendarFragment

--- app/src/main/java/com/yashinsergey/clinic/ui/views/DoctorViewItem.kt
+++ app/src/main/java/com/yashinsergey/clinic/ui/views/DoctorViewItem.kt
@@ -5,13 +5,18 @@ import com.yashinsergey.clinic.R
 import com.yashinsergey.clinic.common.loadImage
 import com.yashinsergey.clinic.databinding.ItemDoctorBinding
 import com.yashinsergey.clinic.model.repos.network.json.Doctor
+import com.yashinsergey.clinic.ui.ButtonId
+import io.reactivex.subjects.PublishSubject
 
-class DoctorViewItem(id: Long, private val doctor: Doctor): BindableItem<ItemDoctorBinding>(id) {
+class DoctorViewItem(id: Long, private val doctor: Doctor, val clickSubject: PublishSubject<ButtonId>): BindableItem<ItemDoctorBinding>(id) {
 
     override fun getLayout(): Int = R.layout.item_doctor
 
     override fun bind(viewBinding: ItemDoctorBinding, position: Int) {
         viewBinding.item = doctor
         loadImage(doctor.photoUrl, viewBinding.itemImage)
+        viewBinding.itemRippleBackground.setOnClickListener {
+            clickSubject.onNext(ButtonId.CALENDAR)
+        }
     }
 }
